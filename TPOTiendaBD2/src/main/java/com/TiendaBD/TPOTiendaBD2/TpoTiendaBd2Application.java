f967c61b1592090c4ca6e1b0bb260b3ed3c0aeaf
Lucas <lucasvonelm@gmail.com>
2023-06-08 19:46:57 -0300

    Iniciado proyecto

---
+++ TPOTiendaBD2/src/main/java/com/TiendaBD/TPOTiendaBD2/TpoTiendaBd2Application.java
@@ -0,0 +1,13 @@
+package com.TiendaBD.TPOTiendaBD2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoTiendaBd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoTiendaBd2Application.class, args);
+	}
+
+}
---
+++ TPOTiendaBD2/src/test/java/com/TiendaBD/TPOTiendaBD2/TpoTiendaBd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaBD.TPOTiendaBD2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoTiendaBd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
